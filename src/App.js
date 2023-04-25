87bb585827b26b403dd640747bcbeae3e2b98c5c
Jose Reyna <jose-atil@hotmail.com>
2023-04-25 11:17:15 -0700

    App.js y Index.js Antes de corregir errores

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+const todos = [
+  { text:'Cortar Cebolla', completed: false},
+  { text:'Tomar curso', completed: false},
+  { text:'LLorar con la cebolla', completed: false}
+]
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<React.Fragment>
+{/*<TodoCounter/>*/}
+    <h2>Has completado 2 de 3 TODOs</h2>
+
+    {/*<TodoSearch/>*/}
+    <imput placeholder="Cebolla" />
+
+    {/*<TodoList>
+      {todos.map(todo => (
+    <TodoItem />
+    ))}
+      </TodoList>*/}
+
+    {/*<CreateTodoButton />*/}
+    <button>+</button>
+</React.Fragment>
   );
 }
 
