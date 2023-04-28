e51105852b7275374188a4b6ecd4b184f3ea167f
Jose Reyna <jose-atil@hotmail.com>
2023-04-28 14:06:58 -0700

    Correcion de archivos para comenzar de nuez

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import react from "react";
-import { TodoCounter } from "./TodoCounter";
-import { TodoSearch } from "./TodoSearch.js";
-import { TodoList } from "./TodoList.js";
-import { TodoItem } from "./TodoItem.js";
-import { CreateTodoButton } from "./TodoCreateButton";
-//import './App.css';
-const todos=[
-  {text:'Cortar cebolla', completed:false},
-  {text:'Tormar el curso de intro a react', completed:false},
-  {text:'Llorar con la llorona', completed:false}
-];
-function App() {
+import logo from './logo.svg';
+import './App.css';
+
+function App(props) {
   return (
-   <react.Fragment>
-      <TodoCounter />    
-      <TodoSearch />
-      <TodoList>
-        {todos.map(todo =>(<TodoItem key={todo.text} text={todo.text} />))}
-      </TodoList>
-      <CreateTodoButton />      
-   </react.Fragment>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {props.children}
+        </a>
+      </header>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+ReactDOM.render(
+  <App saludo="oli">
+    <h1>
+      Jose
+    </h1>
+  </App>,
+  document.getElementById('root')
+);
